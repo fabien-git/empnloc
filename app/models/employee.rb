6177b4c107ab0a8fcf91634a9ba25dd81c304467
Ingrid Gustave <ingrid.gustave@gmail.com>
2024-02-20 11:39:10 +0100

    add description to employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,6 +4,7 @@ class Employee < ApplicationRecord
 
   validates :first_name, :last_name, presence: true
   validates :job_title, length: { minimum: 4 }, presence: true
+  validates :description, length: { minimum: 6 }
   # validates :avatar_img , presence: true
   validates :price_per_day, numericality: { only_integer: true }, presence: true
 end
