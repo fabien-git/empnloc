b7e22e61f4e614e3b9d6218bba7b5bd8699f9e5f
laura vidal <lauravidal.vl@gmail.com>
2024-02-22 05:38:32 +0100

    stimulus picking avatar for creating employee

---
+++ app/javascript/controllers/select_image_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="select-image"
+export default class extends Controller {
+  static targets = [ "displayElement" ]
+
+  pick(event) {
+    console.log("Picking an image")
+    console.log(this.element)
+    console.log(event.originalTarget.outerHTML)
+    this.displayElementTarget.innerHTML = ""
+    this.displayElementTarget.insertAdjacentHTML('beforeend', `<div class="card-avatar">${event.originalTarget.outerHTML}</div>` )
+  }
+}
