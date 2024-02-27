448bd675c89011108afa9284e88ea1339eb1422e
fabien pittiani <fabien.pittiani@gmail.com>
2024-02-27 14:38:58 +0100

    ajout recherche  + anim

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -14,34 +14,30 @@ class EmployeesController < ApplicationController
     @employee_roulette = Employee.find_by(job_title: employee_name)
 
     if params[:query].present?
-      puts "------------------------"
-      puts "------------------------"
-      puts params[:query]
       sql_query = "job_title ILIKE :query OR first_name ILIKE :query OR last_name ILIKE :query"
-      @employees = Employee.where(sql_query, query: "%#{params[:query]}%")
+      @search_employees = Employee.where(sql_query, query: "%#{params[:query]}%")
     else
-      @employees = Employee.all
+      @search_employees = Employee.all
+    end
+
+
+  respond_to do | format |
+    format.html
+      format.text do
+        render partial: "top", locals: { top_employees: @search_employees }, formats: :html
+      end
     end
   end
 
 
  def fetch_employee_data
-    # Afficher les noms des paramètres reçus dans la console
-    puts "Noms des paramètres reçus : #{params.keys}"
-
-    # Récupérer le nom de l'employé à partir des paramètres
     employee_name = params[:data]
-
-    # Effectuer la recherche de l'employé dans la base de données
     employee = Employee.find_by(job_title: employee_name)
-
-    # Renvoyer les données de l'employé en format JSON
     render json: { employee: employee }
   end
 
 
   def show
-
     @employee = Employee.find(params[:id])
     @appointment = Appointment.new
     @reviews = Review.where(employee: params[:id])
@@ -49,12 +45,10 @@ class EmployeesController < ApplicationController
     total_rating = 0
     if @reviews.present?
       @reviews.each do |review|
-      total_rating += review.rating
+        total_rating += review.rating
+      end
+      @average_rating = (total_rating / @reviews.count).round
     end
-    @average_rating = (total_rating / @reviews.count).round
-  end
-
-
   end
 
   def new
@@ -63,6 +57,7 @@ class EmployeesController < ApplicationController
 
   def create
     @employee = Employee.new(employee_params)
+    @employee.rating = 0
     @employee.avatar.attach(io: URI.open(params[:employee][:avatar]), filename: "jean.jpg", content_type: "image/jpg")
     # @employee.avatar.attach(io: URI.open(), filename: "jean.jpg", content_type: "image/jpg")
     @employee.user = current_user
